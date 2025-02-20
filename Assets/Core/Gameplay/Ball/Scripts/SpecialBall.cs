ed982e63599bbe55a97f2062747e14fd07fa7fba
Noam Riahi <noam@baba-ent.com>
2025-02-20 22:09:48 +0200

    add dotween

--- Assets/Core/Gameplay/Ball/Scripts/SpecialBall.cs
+++ Assets/Core/Gameplay/Ball/Scripts/SpecialBall.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SpecialBall : MonoBehaviour
+namespace Core.Balls
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SpecialBall : Ball
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
